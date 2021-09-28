b517e673ae69b30fa726046ae8b984a0a6815d94
Alexander <info@protectyoursite.ru>
2021-09-28 11:04:12 +0300

    settngs improvements

--- administrator/views/settings/tmpl/default_general.php
+++ administrator/views/settings/tmpl/default_general.php
@@ -9,40 +9,42 @@
  * @license GNU/GPL: http://www.gnu.org/copyleft/gpl.html
  */
 
+use Joomla\CMS\Language\Text;
+
 defined('_JEXEC') or die;
 ?>
 <div class="row-fluid">
 	<div class="span6">
-		<fieldset class="form-horizontal">
-			<legend><?php echo JText::_('A_CATEGORIES'); ?></legend>
-			<?php foreach ($this->form->getFieldset('categories') as $field) : ?>
-				<div class="control-group">
-					<div class="control-label">
-						<?php echo $field->label; ?>
-					</div>
-					<div class="controls">
-						<?php echo $field->input; ?>
-					</div>
-				</div>
-			<?php endforeach; ?>
+		<fieldset class="options-form">
+			<legend><?php echo Text::_('A_CATEGORIES'); ?></legend>
+            <?php foreach ($this->form->getFieldset('categories') as $field) : ?>
+                <div class="control-group">
+                    <div class="control-label">
+                        <?php echo $field->label; ?>
+                    </div>
+                    <div class="controls">
+                        <?php echo $field->input; ?>
+                    </div>
+                </div>
+            <?php endforeach; ?>
 		</fieldset>
-		<fieldset class="form-horizontal">
-			<legend><?php echo JText::_('A_MISC'); ?></legend>
-			<?php foreach ($this->form->getFieldset('misc') as $field) : ?>
-				<div class="control-group">
-					<div class="control-label">
-						<?php echo $field->label; ?>
-					</div>
-					<div class="controls">
-						<?php echo $field->input; ?>
-					</div>
-				</div>
-			<?php endforeach; ?>
+		<fieldset class="options-form">
+			<legend><?php echo Text::_('A_MISC'); ?></legend>
+            <?php foreach ($this->form->getFieldset('misc') as $field) : ?>
+                <div class="control-group">
+                    <div class="control-label">
+                        <?php echo $field->label; ?>
+                    </div>
+                    <div class="controls">
+                        <?php echo $field->input; ?>
+                    </div>
+                </div>
+            <?php endforeach; ?>
 		</fieldset>
 	</div>
 	<div class="span6">
-		<fieldset class="form-horizontal">
-			<legend><?php echo JText::_('A_NOTIFICATIONS'); ?></legend>
+		<fieldset class="options-form">
+			<legend><?php echo Text::_('A_NOTIFICATIONS'); ?></legend>
 			<?php foreach ($this->form->getFieldset('notifications') as $field) : ?>
 				<div class="control-group">
 					<div class="control-label">
@@ -54,8 +56,8 @@ defined('_JEXEC') or die;
 				</div>
 			<?php endforeach; ?>
 		</fieldset>
-		<fieldset class="form-horizontal">
-			<legend><?php echo JText::_('A_REPORTS'); ?></legend>
+		<fieldset class="options-form">
+			<legend><?php echo Text::_('A_REPORTS'); ?></legend>
 			<?php foreach ($this->form->getFieldset('reports') as $field) : ?>
 				<div class="control-group">
 					<div class="control-label">
