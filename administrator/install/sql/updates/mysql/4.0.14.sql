5539ad56bd4b8344c00c4ffae669078ccacf1682
Vladimir <web@libra.ms>
2022-01-04 10:47:17 +0300

    Update 4.0.14.sql

--- administrator/install/sql/updates/mysql/4.0.14.sql
+++ administrator/install/sql/updates/mysql/4.0.14.sql
@@ -1,7 +1,6 @@
 ALTER TABLE `#__jcomments` CHANGE `date` `date` DATETIME NOT NULL, CHANGE `checked_out` `checked_out` INT(11) UNSIGNED NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL;
 ALTER TABLE `#__jcomments_blacklist` CHANGE `created` `created` DATETIME NOT NULL, CHANGE `expire` `expire` DATETIME NOT NULL, CHANGE `checked_out` `checked_out` INT(11) UNSIGNED NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL;
 ALTER TABLE `#__jcomments_custom_bbcodes` CHANGE `checked_out` `checked_out` INT(11) UNSIGNED NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL;
-ALTER TABLE `#__jcomments_objects` CHANGE `created` `created` DATETIME NOT NULL;
 ALTER TABLE `#__jcomments_objects` CHANGE `modified` `modified` DATETIME NOT NULL;
 ALTER TABLE `#__jcomments_reports` CHANGE `date` `date` DATETIME NOT NULL;
 ALTER TABLE `#__jcomments_smilies` CHANGE `checked_out` `checked_out` INT(11) UNSIGNED NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL;
