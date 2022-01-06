daba7bead3cde14790f3823b0e3912ba79b76e65
Alexander <info@protectyoursite.ru>
2022-01-06 21:36:38 +0300

    fix mysql updates

--- administrator/install/sql/updates/mysql/4.0.14.sql
+++ administrator/install/sql/updates/mysql/4.0.14.sql
@@ -1,5 +1,5 @@
-ALTER TABLE `#__jcomments` CHANGE `date` `date` DATETIME NOT NULL, CHANGE `checked_out` `checked_out` INT(11) UNSIGNED NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL;
-ALTER TABLE `#__jcomments_blacklist` CHANGE `created` `created` DATETIME NOT NULL, CHANGE `expire` `expire` DATETIME NOT NULL, CHANGE `checked_out` `checked_out` INT(11) UNSIGNED NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL;
+ALTER TABLE `#__jcomments` CHANGE `checked_out` `checked_out` INT(11) UNSIGNED NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL;
+ALTER TABLE `#__jcomments_blacklist` CHANGE `checked_out` `checked_out` INT(11) UNSIGNED NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL;
 ALTER TABLE `#__jcomments_custom_bbcodes` CHANGE `checked_out` `checked_out` INT(11) UNSIGNED NULL, CHANGE `checked_out_time` `checked_out_time` DATETIME NULL;
 ALTER TABLE `#__jcomments_mailq` CHANGE `created` `created` DATETIME NOT NULL;
 ALTER TABLE `#__jcomments_objects` CHANGE `modified` `modified` DATETIME NOT NULL;
